deb348e6d447eb818a6d49467924df06831e8889
Aishwarya <amuthyala511@gmail.com>
2020-11-08 19:35:44 +0530

    Welcome to Hotel Reservation System

---
+++ HotelReservationSystem/src/main/java/com/blz/java/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.blz.java;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO HOTEL RESERVATION PROGRAM IN HOTEL-RESERVATION CLASS");
+	}
+}
